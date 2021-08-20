da5f2c55a4f17b4515b4676ae462f48726a01923
Michal Maciejewski <michal.mac@gmail.com>
2021-08-20 15:42:14 +0200

    collect both moving and stuck agents

--- src/main/java/playground/michalm/util/MovingAgentsRegister.java
+++ src/main/java/playground/michalm/util/MovingAgentsRegister.java
@@ -1,9 +1,9 @@
-/* *********************************************************************** *
+/*
+ * *********************************************************************** *
  * project: org.matsim.*
- *                                                                         *
  * *********************************************************************** *
  *                                                                         *
- * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ * copyright       : (C) 2021 by the members listed in the COPYING,        *
  *                   LICENSE and WARRANTY file.                            *
  * email           : info at matsim dot org                                *
  *                                                                         *
@@ -15,13 +15,13 @@
  *   (at your option) any later version.                                   *
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
- * *********************************************************************** */
+ * *********************************************************************** *
+ */
 
 package playground.michalm.util;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
@@ -33,30 +33,44 @@ import org.matsim.api.core.v01.events.handler.PersonStuckEventHandler;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.events.handler.EventHandler;
+import org.matsim.core.utils.collections.Tuple;
 
 public class MovingAgentsRegister
 		implements EventHandler, PersonDepartureEventHandler, PersonStuckEventHandler, PersonArrivalEventHandler {
-	private final Map<Id<Person>, PersonDepartureEvent> movingAgentsMap = new HashMap<>();
+	private final Map<Id<Person>, PersonDepartureEvent> movingAgentsMap = new LinkedHashMap<>();
+	private final Map<Id<Person>, Tuple<PersonDepartureEvent, PersonStuckEvent>> stuckAgentsMap = new LinkedHashMap<>();
 
 	@Override
 	public void handleEvent(PersonDepartureEvent event) {
+		if (event.getPersonId().toString().equals("0052514")) {
+			System.err.println(event);
+		}
 		movingAgentsMap.put(event.getPersonId(), event);
 	}
 
 	@Override
 	public void handleEvent(PersonArrivalEvent event) {
+		if (event.getPersonId().toString().equals("0052514")) {
+			System.err.println(event);
+		}
 		movingAgentsMap.remove(event.getPersonId());
 	}
 
 	@Override
 	public void handleEvent(PersonStuckEvent event) {
-		// throw new RuntimeException();
-		System.err.println("AgentStuckEvent:");
-		System.err.println(event);
+		if (event.getPersonId().toString().equals("0052514")) {
+			System.err.println(event);
+		}
+		var departureEvent = movingAgentsMap.remove(event.getPersonId());
+		stuckAgentsMap.put(event.getPersonId(), new Tuple<>(departureEvent, event));
 	}
 
-	public Set<Id<Person>> getMovingAgentIds() {
-		return movingAgentsMap.keySet();
+	public Map<Id<Person>, PersonDepartureEvent> getMovingAgents() {
+		return movingAgentsMap;
+	}
+
+	public Map<Id<Person>, Tuple<PersonDepartureEvent, PersonStuckEvent>> getStuckAgents() {
+		return stuckAgentsMap;
 	}
 
 	@Override
@@ -64,7 +78,7 @@ public class MovingAgentsRegister
 		movingAgentsMap.clear();
 	}
 
-	private static final MovingAgentsRegister MOVING_AGENTS_REGISTER = new MovingAgentsRegister();
+	public static final MovingAgentsRegister MOVING_AGENTS_REGISTER = new MovingAgentsRegister();
 
 	public static AbstractModule createModule() {
 		return new AbstractModule() {
