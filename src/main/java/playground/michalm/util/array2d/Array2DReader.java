cade6bf52bc9f5507e608b6049028c8fe05af0cb
Michal Maciejewski <michal.mac@gmail.com>
2022-05-27 10:22:51 +0200

    remove unused Array2D* functionality

--- src/main/java/playground/michalm/util/array2d/Array2DReader.java
+++ src/main/java/playground/michalm/util/array2d/Array2DReader.java
@@ -16,112 +16,49 @@ import org.matsim.core.utils.io.IOUtils;
  * @author michalm
  */
 public class Array2DReader {
-	public static final Strategy DOUBLE_STRATEGY = new DoubleStrategy();
-	public static final Strategy INT_STRATEGY = new IntStrategy();
-	public static final Strategy STRING_STRATEGY = new StringStrategy();
-
 	public static double[][] getDoubleArray(String file, int cols) {
-		return (double[][])getArray(file, cols, DOUBLE_STRATEGY);
-	}
-
-	public static int[][] getIntArray(String file, int cols) {
-		return (int[][])getArray(file, cols, INT_STRATEGY);
-	}
-
-	public static String[][] getStringArray(String file, int cols) {
-		return (String[][])getArray(file, cols, STRING_STRATEGY);
-	}
-
-	public static Object getArray(String file, int cols, Strategy strategy) {
 		try (BufferedReader br = IOUtils.getBufferedReader(file)) {
-			return getArray(br, cols, strategy);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+			double[][] result1 = null;
+			List<double[]> rows = new ArrayList<>();
+			boolean endOfArray = false;
 
-	public static Object getArray(BufferedReader br, int cols, Strategy strategy) throws IOException {
-		List<Object> rows = new ArrayList<>();
-		boolean endOfArray = false;
+			for (String line = br.readLine(); line != null; line = br.readLine()) {
+				StringTokenizer st = new StringTokenizer(line, " \t");
 
-		for (String line = br.readLine(); line != null; line = br.readLine()) {
-			StringTokenizer st = new StringTokenizer(line, " \t");
-
-			if (endOfArray) {
-				if (st.hasMoreTokens()) {
-					throw new RuntimeException("Non-empty line after matrix");
-				}
-			} else {
-				if (!st.hasMoreTokens()) {
-					endOfArray = true;
-					continue;
-				}
-
-				Object row = Array.newInstance(strategy.getComponentType(), cols);
-
-				for (int i = 0; i < cols; i++) {
+				if (endOfArray) {
+					if (st.hasMoreTokens()) {
+						throw new RuntimeException("Non-empty line after matrix");
+					}
+				} else {
 					if (!st.hasMoreTokens()) {
-						throw new RuntimeException("Too few elements");
+						endOfArray = true;
+						continue;
 					}
 
-					strategy.addToRow(row, i, st.nextToken());
-				}
+					var row = new double[cols];
 
-				if (st.hasMoreTokens()) {
-					throw new RuntimeException("Too many elements");
-				}
+					for (int i = 0; i < cols; i++) {
+						if (!st.hasMoreTokens()) {
+							throw new RuntimeException("Too few elements");
+						}
+						row[i] = Double.parseDouble(st.nextToken());
+					}
 
-				rows.add(row);
+					if (st.hasMoreTokens()) {
+						throw new RuntimeException("Too many elements");
+					}
+
+					rows.add(row);
+				}
 			}
-		}
 
-		if (rows.size() == 0) {
-			return null;
-		}
+			if (rows.size() != 0) {
+				result1 = rows.toArray(double[][]::new);
+			}
 
-		Object row0 = rows.get(0);
-		Class<?> clazz = row0 != null ? row0.getClass() : Object.class;
-
-		return rows.toArray((Object[])Array.newInstance(clazz, rows.size()));
-	}
-
-	public interface Strategy {
-		Class<?> getComponentType();
-
-		void addToRow(Object row, int col, String element);
-	}
-
-	public static class DoubleStrategy implements Strategy {
-		@Override
-		public Class<?> getComponentType() {
-			return double.class;
-		}
-
-		@Override
-		public void addToRow(Object row, int idx, String element) {
-			((double[])row)[idx] = Double.parseDouble(element);
-		}
-	}
-
-	public static class IntStrategy implements Strategy {
-		@Override
-		public Class<?> getComponentType() {
-			return int.class;
-		}
-
-		public void addToRow(Object row, int idx, String element) {
-			((int[])row)[idx] = Integer.parseInt(element);
-		}
-	}
-
-	public static class StringStrategy implements Strategy {
-		@Override
-		public Class<?> getComponentType() {
-			return String.class;
-		}
-
-		public void addToRow(Object row, int idx, String element) {
-			((String[])row)[idx] = element;
+			return result1;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
